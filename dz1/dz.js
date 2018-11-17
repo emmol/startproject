a4c6d13b0d09ebb63e0891d2119d5e356fc8f8c0
Eugene Ilyushkin <eugene.omsk@gmail.com>
2018-11-17 19:10:57 +0600

    Новая редакция dz

--- dz1/dz.js
+++ dz1/dz.js
@@ -1,22 +1,21 @@
-var money = +prompt("Ваш бюджет на месяц?", "");
-var time = prompt("Введите дату в формате YYYY-MM-DD", "");
-
-var itemOfExpensesOne = prompt("Введите обязательную статью расходов в этом месяце", "");
-var costsOne = +prompt("Во сколько обойдется?", "");
-
-var itemOfExpensesTwo = prompt("Введите обязательную статью расходов в этом месяце", "");
-var costsTwo = +prompt("Во сколько обойдется?", "");
+var money = +prompt("Ваш бюджет на месяц?", ""),
+  time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
 var appData = {
   budget: money,
   timeData: time,
-  expenses: {
-    [itemOfExpensesOne] : costsOne,
-    [itemOfExpensesTwo] : costsTwo,
-  },
+  expenses: {},
   optionalExpenses : {},
   income : [],
   savings : false
 };
+
+var itemOfExpensesOne = prompt("Введите обязательную статью расходов в этом месяце", ""), 
+  costsOne = +prompt("Во сколько обойдется?", ""),
+  itemOfExpensesTwo = prompt("Введите обязательную статью расходов в этом месяце", ""),
+  costsTwo = +prompt("Во сколько обойдется?", "");
+
+appData.expenses.itemOfExpensesOne = costsOne;
+appData.expenses.itemOfExpensesTwo = costsTwo;
 
 alert("Ваш бюджет на один день равен: " + (appData.budget / 30).toFixed(2));
