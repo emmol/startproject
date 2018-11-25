eaaee487bd21a1cde3dd6143276f1ab7178f71a6
Eugene Ilyushkin <eugene.omsk@gmail.com>
2018-11-25 23:09:01 +0600

    Выполнение третьего задания

--- dz3/dz.js
+++ dz3/dz.js
@@ -1,5 +1,6 @@
 let money, time;
 
+// начальная функция, доход, дата 
 function start() {
   money = +prompt("Ваш бюджет на месяц?", "");
   time = prompt("Введите дату в формате YYYY-MM-DD", "");
@@ -10,6 +11,7 @@ function start() {
 };
 start();
 
+// основной объект приложения
 let appData = {
   budget: money,
   timeData: time,
@@ -19,6 +21,7 @@ let appData = {
   savings : true
 };
 
+// функция добавления статей расхода и стоимости
 function chooseExpenses() {
   for (let i = 0; i < 2; i++) {
     let a = prompt("Введите обязательную статью расходов в этом месяце", ""), 
@@ -35,20 +38,28 @@ function chooseExpenses() {
 }
 chooseExpenses();
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) {
-  console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-  console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-  console.log("Высокий уровень достатка");
-} else {
-  console.log("Произошла ошибка");
+// функция проверки размера дневного бюджета
+function detectDayBudget() {
+  appData.moneyPerDay = (appData.budget / 30).toFixed();
+  alert("Ежедневный бюджет: " + appData.moneyPerDay);
 }
+detectDayBudget();
 
+// функция проверки уровня достатка
+function detectLevel() {
+  if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+  } else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+  } else {
+    console.log("Произошла ошибка");
+  }
+}
+detectLevel();
+
+// функция расчета дохода с депозита
 function checkSavings() {
   if (appData.savings == true) {
     let save = +prompt("Какова сумма накоплений?"),
@@ -58,5 +69,15 @@ function checkSavings() {
     alert("Доход в месяц с вашего депозита: " + (appData.monthIncome).toFixed());
   }
 }
-
 checkSavings();
+
+// функция определения не обязательных расходов
+function chooseOptExpenses() {
+  for (let i = 0; i < 3; i++) {
+    let c = prompt("Статья необязательных расходов?");
+    appData.optionalExpenses[i] = c;
+  }
+}
+chooseOptExpenses();
+
+console.log(appData);
